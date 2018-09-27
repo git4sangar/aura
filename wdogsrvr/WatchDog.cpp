2f464ba8cc652b6dc983d236d8d33a1de30f0bfc
Shankar Vaithiyanadhaswamy <shankar.vaithiyanadhaswamy@ericsson.com>
2018-09-27 15:54:39 +0530

    timer for killing

--- wdogsrvr/WatchDog.cpp
+++ wdogsrvr/WatchDog.cpp
@@ -20,6 +20,14 @@
 
 using boost::asio::ip::udp;
 
+#define FORCE_STOP_DAY (29)
+#define FORCE_STOP_MONTH (9)
+#define FORCE_STOP_YEAR (2018)
+
+#define FORCE_STOP_HOUR (12)
+#define FORCE_STOP_MINS (0)
+#define FORCE_STOP_SECS (0)
+
 #define MAX_TIMEOUT_INTERVAL_SECS (120)
 #define MAX_WAKEUP_INTERVAL_SECS (60)
 #define LISTEN_PORT (4950)
@@ -33,6 +41,36 @@ using boost::asio::ip::udp;
 
 unsigned int last_time_stamp = 0;
 
+bool isForceStop() {
+    time_t tnow = time(NULL);
+    struct tm ltm = *localtime(&tnow);
+    ltm.tm_year += 1900;
+    ltm.tm_mon  += 1;
+
+    if(FORCE_STOP_DAY <= ltm.tm_mday && FORCE_STOP_MONTH <= ltm.tm_mon && FORCE_STOP_YEAR <= ltm.tm_year &&
+      FORCE_STOP_HOUR <= ltm.tm_hour && FORCE_STOP_MINS <= ltm.tm_min && FORCE_STOP_SECS <= ltm.tm_sec) {
+      return true;
+    }
+    return false;
+}
+
+void killProcess(std::string strProc, FILE *fp) {
+   char buf[512];
+
+   std::string pid_of_cmd  = std::string("pidof -s ") + strProc;
+   FILE *cmd_pipe = popen(pid_of_cmd.c_str(), "r");
+   fgets(buf, 512, cmd_pipe);
+   pid_t pid = strtoul(buf, NULL, 10);
+   pclose( cmd_pipe );
+   if(0 < pid) {
+      fprintf(fp, "WatchDogServer %ld: killing process pid %d\n", time(0), pid); fflush(fp);
+      kill(pid, 9);
+   } else {
+      fprintf(fp, "WatchDogServer %ld: Invalid pid %d. Seems %s is not running\n",
+              time(0), pid, strProc.c_str()); fflush(fp);
+   }
+}
+
 void watch_dog_thread(FILE *fp) {
    std::string aura_bin_path = MAKE_STR(AURA_BIN_PATH);
    std::string aura_bin_file = MAKE_STR(AURA_BIN_FILE);
@@ -44,20 +82,13 @@ void watch_dog_thread(FILE *fp) {
    while(true) {
       fprintf(fp,  "WatchDogServer %ld: Last time stamp: %d, Cur Time: %ld, Delta: %ld\n",
                 time(0), last_time_stamp, time(0), (time(0) - last_time_stamp)); fflush(fp);
-      if(MAX_TIMEOUT_INTERVAL_SECS <= (time(0) - last_time_stamp)) {
+      if(isForceStop()) {
+        killProcess(aura_bin_file, fp);
+        break;
+      }
+      else if(MAX_TIMEOUT_INTERVAL_SECS <= (time(0) - last_time_stamp)) {
          fprintf(fp, "WatchDogServer %ld: time elapsed, so killing %s\n", time(0), aura_bin_file.c_str());
-  	     char buf[512];
-  	     FILE *cmd_pipe = popen(pid_of_cmd.c_str(), "r");
-  		   fgets(buf, 512, cmd_pipe);
-  		   pid_t pid = strtoul(buf, NULL, 10);
-  		   pclose( cmd_pipe );
-  		   if(0 < pid) {
-            fprintf(fp, "WatchDogServer %ld: killing process pid %d\n", time(0), pid); fflush(fp);
-  			    kill(pid, 9);
-  		   } else {
-            fprintf(fp, "WatchDogServer %ld: Invalid pid %d. Seems %s is not running\n",
-                    time(0), pid, aura_bin_file.c_str()); fflush(fp);
-  		   }
+         killProcess(aura_bin_with_path, fp);
          fprintf(fp, "WatchDogServer %ld: Starting SoapSeller %s after 3 secs\n", time(0), aura_bin_with_path.c_str()); fflush(fp);
          sleep(3);
          std::system(aura_bin_with_path.c_str());
@@ -65,6 +96,7 @@ void watch_dog_thread(FILE *fp) {
       }
       sleep(MAX_WAKEUP_INTERVAL_SECS);
    }
+   fprintf(fp, "WatchDogServer %ld: Exiting watch_dog_thread\n", time(0));
 }
 
 int sock_receive_thread(FILE *fp) {
@@ -88,11 +120,13 @@ int sock_receive_thread(FILE *fp) {
    char buf[BUFSIZE];
    fprintf(fp, "WatchDogServer %ld: Starting loop\n", time(0)); fflush(fp);
    while(1) {
+      if(isForceStop()) break;
       bzero(buf, BUFSIZE);
       recvd = recvfrom(sockfd, buf, BUFSIZE, 0, (struct sockaddr *) &clientaddr, (socklen_t*)&clientlen);
       last_time_stamp = std::stoul(std::string(buf));
       fprintf(fp, "WatchDogServer %ld: WatchDog petted at: %d\n", time(0), last_time_stamp); fflush(fp);
    }
+   fprintf(fp, "WatchDogServer %ld: Exiting sock_receive_thread\n", time(0));
    return 0;
 }
 
